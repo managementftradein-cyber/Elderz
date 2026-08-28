d43bc27389ba537004cfcc3596148b1435c0922e
managementftradein-cyber <managementftradein@gmail.com>
2026-08-28 17:22:33 -0300

    Delete lib directory

--- lib/supabase-admin(1).ts
+++
@@ -1,3 +0,0 @@
-import {createClient} from "@supabase/supabase-js";
-export const supabaseAdmin=createClient(process.env.SUPABASE_URL!,process.env.SUPABASE_SERVICE_ROLE_KEY!,{auth:{autoRefreshToken:false,persistSession:false}});
-export const bucket=process.env.SUPABASE_STORAGE_BUCKET||"site-media";
